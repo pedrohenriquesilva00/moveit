072a8a7400c65fe9c52d6811f21259f724f37ca6
Pedro Henrique da Silva <pedro.silva301201@gmail.com>
2021-03-01 19:43:16 -0300

    Up

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,17 @@
+import { readSync } from "fs";
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+
+import styles from '../styles/components/CompletedChallenges.module.css';
+
+export function CompletedChallenges() {
+
+    const { challengesCompleted } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.completedChallengesContainer}>
+            <span>Desafios completos</span>
+            <span>{challengesCompleted}</span>
+        </div>
+    );
+}
